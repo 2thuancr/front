479815c871bd2fe71fc86c456cac2fa3a807c9f6
2ThuanCr <2thuancr03@gmail.com>
2025-10-24 21:35:51 +0700

    add Google Analytics

---
+++ src/lib/gtag.ts
@@ -0,0 +1,29 @@
+// This function is used to initialize Google Analytics
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || '';
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  if (typeof window !== 'undefined' && (window as any).gtag) {
+    (window as any).gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+type EventProps = {
+  action: string;
+  category: string;
+  label: string;
+  value?: number;
+};
+
+export const event = ({ action, category, label, value }: EventProps) => {
+  if (typeof window !== 'undefined' && (window as any).gtag) {
+    (window as any).gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+};
